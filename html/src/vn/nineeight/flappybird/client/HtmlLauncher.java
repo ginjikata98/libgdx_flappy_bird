e156d77db444cfb720276ec7d86c84e6c72ca0bd
Tony <dung.vu@finhay.com.vn>
2022-11-03 08:43:34 +0700

    init

---
+++ html/src/vn/nineeight/flappybird/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package vn.nineeight.flappybird.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import vn.nineeight.flappybird.FlappyBird;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new FlappyBird();
+        }
+}
